d581ce96315e492603449a369d260025d56edc67
Grongr <53980840+Grongr@users.noreply.github.com>
2020-10-12 12:35:49 +0300

    Add files via upload

--- quadratic_equation.c
+++ quadratic_equation.c
@@ -5,6 +5,7 @@
 #include <assert.h>
 
 #define epsilon 1e-10
+#define SIZE_OF_LINE 200
 
 //--------------------------------------------------------------//
 //!        Prints some information and instructions
@@ -104,9 +105,10 @@ void   check_unit_test(double x1, double x2, int nroots, const double tx1,
 
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
-int main() {
-
-  if (hello()) {
+int main()
+{
+  if (hello())
+  {
     return 0;
   }
 
@@ -125,8 +127,8 @@ int main() {
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
 
-int hello() {
-
+int hello()
+{
   char command = 0;
 
   printf("\n=====================================================================\n");
@@ -138,45 +140,53 @@ int hello() {
   printf("Your command: ");
 
   command = getchar();
-  if (getchar() != '\n')
-    scanf("%*s");
+  while (getchar() != '\n');
 
-  while (command != 'h' && command != 'u' && command != 'c') {
-
+  while (command != 'h' && command != 'u' && command != 'c')
+  {
     elementary_error();
 
     command = getchar();
-    if(getchar() != '\n') scanf("%*s");
+    while (getchar() != '\n');
   } 
 
-  if (command == 'h') {
-
+  if (command == 'h')
+  {
     instructions();
-
-  } else if (command == 'u') {
-    
+  }
+  else if (command == 'u')
+  {
     unit_tests();
-
     return 1;
   }
   
   return 0;
 }
 
-void get_coefficients(double* a, double* b, double* c) {
-
-  assert(!(a == b));
-  assert(!(b == c));
+void get_coefficients(double* a, double* b, double* c)
+{
+  assert(a != b);
+  assert(b != c);
 
   printf("\n==================================\n");
   printf("Enter the coefficients of equation:\n");
 
-  int count_of_normal_inputs = scanf("%lg %lg %lg", a, b, c);
+  int count_of_normal_inputs = 0;
 
-  while (count_of_normal_inputs != 3) {
-    printf("\n==================================\n");
-    printf("An input is not a numbers set.\nPlease, enter the coefficientd one more time:\n");
+  while (count_of_normal_inputs != 3)
+  {
     count_of_normal_inputs = scanf("%lg %lg %lg", a, b, c);
+    if (count_of_normal_inputs == 3)
+    {
+      break;
+    }
+    else
+    {
+      while (getchar() != '\n');
+
+      printf("\n==================================\n");
+      printf("An input is not a numbers set.\nPlease, enter the coefficientd one more time:\n");
+    }
   }
 
   assert(isfinite(*a));
@@ -186,27 +196,29 @@ void get_coefficients(double* a, double* b, double* c) {
   return;
 }
 
-void instructions() {
-
+void instructions()
+{
   FILE *fp = fopen("./instructions.txt", "r");
 
-  if (fp == NULL) {
+  if (fp == NULL)
+  {
     printf("Something went wrong.\nCannot open file <instructions.txt>\n");
     printf("Place such a file in the same directory as an executable file\nand reboot the process.\n");
     printf("Or continue without instructions T_T\n");
     return;
   }
 
-  char line[200];
-  while (fgets(line, 200, fp)) {
+  char line[SIZE_OF_LINE];
+  while (fgets(line, SIZE_OF_LINE, fp))
+  {
     printf("%s", line);
   }
 
   return;
 }
 
-void elementary_error() {
-
+void elementary_error()
+{
   printf("\n=====================================================================\n");
   printf("Warning: wrong input, please try one more time.\nDo your best!\n");
   printf("=====================================================================\n");
@@ -218,24 +230,25 @@ void elementary_error() {
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
 
-int cmp(double a, double b) {
-  
-  if (a - b <= epsilon)
+int cmp(double a, double b)
+{
+  if (a - b <= epsilon)       /* if a = b  */
     return 0;
-  else if (b + epsilon < a)
+  else if (b + epsilon < a)   /* if a > b  */
     return 1;
-  else if (a + epsilon < b)
+  else if (a + epsilon < b)   /* if a < b  */
     return -1;
 }
 
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
 
-int get_equation_result(double a, double b, double c, double* x1, double* x2) {
+int get_equation_result(double a, double b, double c, double* x1, double* x2)
+{
+  assert(x1 != x2);
 
-  assert(!(x1 == x2));
-
-  if (a < 0) {
+  if (a < 0)
+  {
     a *= -1;
     b *= -1;
     c *= -1;
@@ -245,22 +258,21 @@ int get_equation_result(double a, double b, double c, double* x1, double* x2) {
 
   D = calculate_the_discrimimant(a, b, c);
 
-  if (!cmp(a, 0.0)) {
-
+  if (!cmp(a, 0.0))                                   /* linear */
+  {
     return calculate_linear_equation(b, c, x1);
-
-  } else if (!cmp(D, 0.0)) {
-
+  }
+  else if (!cmp(D, 0.0))                              /* D = 0 */
+  {
     calculate_linear_equation(2 * a, b, x1);
-
     return 1;
-
-  } else if (!cmp(D, -1.0)) {
-
+  }
+  else if (!cmp(D, -1.0))                             /* D < 0 */
+  {
     return 0;
-
-  } else if (cmp(D, 0.0) == 1) {
-
+  }
+  else if (cmp(D, 0.0) == 1)                          /* D > 0 */
+  {
     calculate_linear_equation(2 * a, (b + D), x1);
 
     calculate_linear_equation(2 * a, (b - D), x2);
@@ -269,50 +281,48 @@ int get_equation_result(double a, double b, double c, double* x1, double* x2) {
   } 
 }
 
-double calculate_the_discrimimant(double a, double b, double c) {
-
+double calculate_the_discrimimant(double a, double b, double c)
+{
   assert(isfinite(b * b));
   assert(isfinite(4 * a * c));
 
   double D = b * b - 4 * a * c;
 
-  if (cmp(D, 0.0) == 1 || !cmp(D, 0.0)) {
-
+  if (cmp(D, 0.0) == 1 || !cmp(D, 0.0))
+  {
     return sqrt(D);
-
-  } else {
-    
-    return -1;
-
   }
-
+  else
+  {
+    return -1;
+  }
 }
 
-int calculate_linear_equation(double b, double c, double* x1) {
-  
-  if (!cmp(b, 0.0) && !cmp(c, 0.0)) {
-
+int calculate_linear_equation(double b, double c, double* x1)
+{
+  if (!cmp(b, 0.0) && !cmp(c, 0.0))
+  {
     return -1;
-
-  } else if (!cmp(b, 0.0)) {
-
+  }
+  else if (!cmp(b, 0.0))
+  {
     return 0;
-
-  } else {
-
+  }
+  else
+  {
     *x1 = -c / b;
-
     return 1;
   }
 }
 
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
-void print_result(double x1, double x2, int number_of_roots) {
-
+void print_result(double x1, double x2, int number_of_roots)
+{
   printf("\n==================================\n");
 
-  switch (number_of_roots) {
+  switch (number_of_roots)
+  {
     case  1: 
       printf("There is only one solution for your equation.\n");
       printf("x = %.4lg\n", x1);
@@ -337,21 +347,23 @@ void print_result(double x1, double x2, int number_of_roots) {
       abort();
     break;
   }
-
 }
 
 
 // Unit Tests
 // ---------------------------------------------------------------
 
-void check_unit_test(double x1, double x2, int nroots, const double tx1, const double tx2, const int tnroots, char* p, int test_number) {
-  if (cmp(tx1, x1) || cmp(tx2, x2)) {
+void check_unit_test(double x1, double x2, int nroots, const double tx1, const double tx2, const int tnroots, char* p, int test_number)
+{
+  if (cmp(tx1, x1) || cmp(tx2, x2))
+  {
     printf("\n==================================\n");
     printf("Error in unitests. Test %d failed.\n", test_number);
     printf("Result is %.4lg and %.4lg, but needed:\n%.4lg and %.4lg\n", x1, x2, tx1, tx2);
     *p = 0;
   }
-  if (cmp(tnroots, nroots)) {
+  if (cmp(tnroots, nroots))
+  {
     printf("Error in unitests. Test %d failed.\n", test_number);
     printf("Number of roots needs to be %d, but it is %d\n", tnroots, nroots);
     *p = 0;
@@ -359,6 +371,7 @@ void check_unit_test(double x1, double x2, int nroots, const double tx1, const d
 }
 
 #define TEST(NUM_TEST, A, B, C, X1, X2, NUM_ROOTS)                                               \
+  {                                                                                              \
     test_number = NUM_TEST;                                                                      \
     double a = A, b = B, c = C, x1 = 0, x2 = 0;                                                  \
     int nroots = get_equation_result(a, b, c, &x1, &x2);                                         \
@@ -366,39 +379,24 @@ void check_unit_test(double x1, double x2, int nroots, const double tx1, const d
     const int true_nroots = NUM_ROOTS;                                                           \
     const double true_x1  = X1;                                                                  \
     const double true_x2  = X2;                                                                  \
-    check_unit_test(x1, x2, nroots, true_x1, true_x2, true_nroots, &passed, test_number);        
+    check_unit_test(x1, x2, nroots, true_x1, true_x2, true_nroots, &passed, test_number);        \
+  }
 
-void unit_tests() {
+void unit_tests()
+{
   char passed = 1;
   int test_number = 0;
 
-  //     num     a     b      c    x1   x2  numOfRoot
-  {
-    TEST(  1, -1.0, -2.0,  15.0, -5.0, 3.0,         2)
-  }
-
-  //     num     a     b      c    x1   x2  numOfRoot
-  {
-    TEST(  2,  0.0,  1.0,   1.0, -1.0, 0.0,         1)
-  }
-
-  //     num     a     b      c    x1   x2  numOfRoot
-  {
-    TEST(  3,  1.0, 12.0,  36.0, -6.0, 0.0,         1)
-  }
-
-  //     num     a     b      c    x1   x2  numOfRoot
-  {
-    TEST(  4,  1.0,  0.0, -16.0, -4.0, 4.0,         2)
-  }
-
-  //     num     a     b      c    x1   x2  numOfRoot
-  {
-    TEST(  5,  0.0,  0.0,   0.0,  0.0, 0.0,        -1)
-  }
+  //   num     a     b      c    x1   x2  numOfRoot
+  TEST(  1, -1.0, -2.0,  15.0, -5.0, 3.0,         2)
+  TEST(  2,  0.0,  1.0,   1.0, -1.0, 0.0,         1)
+  TEST(  3,  1.0, 12.0,  36.0, -6.0, 0.0,         1)
+  TEST(  4,  1.0,  0.0, -16.0, -4.0, 4.0,         2)
+  TEST(  5,  0.0,  0.0,   0.0,  0.0, 0.0,        -1)
 
   #undef TEST
-  if (passed) {
+  if (passed)
+  {
     printf("\n==================================\n");
     printf("Tests passed without problems.\n");
   }
